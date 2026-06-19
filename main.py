4021f855e96dc6321d2b828a2fe6d98ce24cdc58
zafathoni <za.fathoni2@gmail.com>
2026-06-19 18:26:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import os
 import requests
 
+API_KEY = os.environ["GOOGLE_API_KEY"]
+CX = os.environ["GOOGLE_CX"]
+
 BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-message = """
-🩺 OBGYN Vacancy Radar
+query = "dokter spesialis obgyn"
 
-Bot berhasil berjalan dari GitHub Actions.
-"""
+url = "https://www.googleapis.com/customsearch/v1"
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+response = requests.get(
+    url,
+    params={
+        "key": API_KEY,
+        "cx": CX,
+        "q": query,
+        "num": 5
+    }
+)
+
+data = response.json()
+
+message = "🩺 TEST HASIL PENCARIAN\n\n"
+
+if "items" in data:
+    for item in data["items"]:
+        title = item.get("title", "")
+        link = item.get("link", "")
+
+        message += f"• {title}\n{link}\n\n"
+else:
+    message += str(data)
+
+telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 requests.post(
-    url,
+    telegram_url,
     json={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": message[:4000]
     }
 )
